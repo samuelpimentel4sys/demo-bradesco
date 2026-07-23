f2e9690e1e9dea4c8e420649c6cfcc734a91efac
samuelpimentel4sys <158058005+samuelpimentel4sys@users.noreply.github.com>
2026-07-23 11:32:05 -0300

    chore: adicionar classe principal PortalNegociacaoApplication.java

---
+++ backend-java/src/main/java/com/foursys/portalnegociacao/PortalNegociacaoApplication.java
@@ -0,0 +1,11 @@
+package com.foursys.portalnegociacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalNegociacaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortalNegociacaoApplication.class, args);
+    }
+}
